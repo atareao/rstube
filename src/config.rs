94ccbf3bdfa4bcae2270f1b512b75baa662a0d56
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-11-21 07:42:22 +0100

    users

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,4 @@
+use actix_web_httpauth::extractors::basic::BasicAuth;
 use serde::{Serialize, Deserialize};
 use serde_yaml::Error;
 
@@ -29,11 +30,47 @@ impl Configuration {
     }
 
     pub fn get_user(&self, username: &str) -> Option<&User>{
-        for user in self.users{
+        for user in self.users.as_slice(){
             if username == user.username{
                 return Some(&user);
             }
         }
         None
+    }
+
+    pub fn check_basic_auth(&self, basic_auth: &BasicAuth) -> bool{
+        let username = basic_auth.user_id();
+        match basic_auth.password(){
+            Some(p) => self.check_user(username, p),
+            None => false,
+        }
+    }
+
+    pub fn check_user(&self, username: &str, password: &str) -> bool{
+        for user in self.users.as_slice(){
+            if username == user.username &&
+                    md5_hash(password) == user.password{
+                return true;
+            }
+        }
+        false
+    }
+}
+
+fn md5_hash(to_hash: &str) -> String{
+    let digest = md5::compute(to_hash);
+    format!("{:x}", digest)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::config::md5_hash;
+
+    #[test]
+    fn it_works() {
+        let sample = "sample to digest";
+        let hash = md5_hash(sample);
+        println!("{}", hash);
+        assert_eq!(hash, "c0a5b64fb550f35d255f2033fdc57419");
     }
 }
